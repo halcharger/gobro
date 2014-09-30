43d1f140bce5275400250b615da4d0152c7d9d02
halcharger <allen.firth@gmail.com>
2014-09-30 13:01:52 +0200

    adding initial asp.net mvc website template

---
+++ GoBro.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoBro.Web.Startup))]
+namespace GoBro.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
