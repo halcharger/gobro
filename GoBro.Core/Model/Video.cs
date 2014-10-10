85bde3c8916f0928ed50bf66aa0944f60481350a
halcharger <allen.firth@gmail.com>
2014-10-10 16:32:42 +0200

    adding core assembly and Video TableEntity

---
+++ GoBro.Core/Model/Video.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoBro.Core.Model
+{
+    public class Video : TableEntity
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string UserId { get; set; }
+        public string YoutubeLink { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
