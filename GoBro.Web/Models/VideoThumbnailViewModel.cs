b125ccf12dec7d58562527a44352996a4372ddea
halcharger <allen.firth@gmail.com>
2014-10-15 11:48:47 +0200

    adding youtube thumbnail images

--- GoBro.Web/Models/VideoThumbnailViewModel.cs
+++ GoBro.Web/Models/VideoThumbnailViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NExtensions;
 
 namespace GoBro.Web.Models
 {
@@ -13,5 +14,19 @@ namespace GoBro.Web.Models
         public string YoutubeLink { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public string ThumbnailImageUrl { 
+            get {
+                var youtubeThumbnailImgTemplate = "http://img.youtube.com/vi/{0}/0.jpg";
+
+                if (YoutubeLink.HasValue())
+                {
+                    var youtubeId = HttpUtility.ParseQueryString(new Uri(YoutubeLink).Query).Get("v");
+                    return string.Format(youtubeThumbnailImgTemplate, youtubeId);
+                }
+
+                return youtubeThumbnailImgTemplate;
+            } 
+        }
     }
 }
