e2abd3d11c9bb1b65c6ba07bc567b23807c6b7c1
halcharger <allen.firth@gmail.com>
2014-10-17 11:49:08 +0200

    delete confirmation modal dialog working

--- GoBro.Web/App_Start/BundleConfig.cs
+++ GoBro.Web/App_Start/BundleConfig.cs
@@ -8,9 +8,6 @@ namespace GoBro.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -19,13 +16,13 @@ namespace GoBro.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/upload").Include("~/Scripts/app/upload.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/userVideos").Include("~/Scripts/app/userVideos.js"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
---
+++ GoBro.Web/Scripts/app/userVideos.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    attachConfirmDeleteHandlers();
+});
+
+function attachConfirmDeleteHandlers() {
+    $('a[name="confirmDeleteLink"]').click(function () {
+        var videoId = $(this).attr('data-videoId');
+        confirmDelete(videoId);
+    });
+}
+
+function confirmDelete(videoId) {
+    var videoTitleSelector = '#video_' + videoId + ' div span h4';
+    var videoTitle = $(videoTitleSelector).text();
+    $('.modal-body p').text(videoTitle);
+
+    var videoImgSelector = '#video_' + videoId + ' div a img';
+    var videoImgUrl = $(videoImgSelector).attr('src');
+    $('.modal-body img').attr('src', videoImgUrl);
+
+    $('#confirmDeleteModal').modal('show');
+}
